3ed98ddafde8107b3010101770900dfb3b8e642b
johnlobo <john.lobo@hotmail.com>
2014-11-25 08:09:55 +0100

    cpcrslib2

--- SDCC/make - copia.bat
+++ SDCC/make - copia.bat
@@ -7,10 +7,13 @@ sdasz80 -o Keyboard.s
 sdasz80 -o UnExoOpt.s
 sdasz80 -o Uncrunch.s
 sdasz80 -o GphStrStd.s
+sdasz80 -o TileMap.s
 sdasz80 -o Wyz.s
 
 
 
 sdar rc cpcrslib.lib cpcrslib.rel GphStr.rel Sprites.rel Keyboard.rel UnExoOpt.rel Uncrunch.rel GphStrStd.rel TileMap.rel 
-sdar rc cpcwyzlib.lib Wyz.rel
 
+sdar rc cpcwyzlib.lib Wyz.rel
+copy cpcrslib.lib C:\sdcc\lib\z80
+copy cpcwyzlib.lib C:\sdcc\lib\z80
